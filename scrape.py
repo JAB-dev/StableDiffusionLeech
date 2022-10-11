97b33c16e53a403ecebd59ed63cb73325756c4d3
JAB <68183649+JAB-dev@users.noreply.github.com>
2022-10-11 20:12:06 +0200

    Day1patchomegalul

--- scrape.py
+++ scrape.py
@@ -7,18 +7,18 @@ def scrape(start,end):
         y= requests.get('https://'+ str(x) + '.gradio.app/')
         #check if the response is 200
         if (y.status_code == 200):
-            #Before printing also check if the title of the page from y is Stable Diffusion
+            #TODO Before printing also check if the title of the page from y is Stable Diffusion
             print('https://'+ str(x) + '.gradio.app/')
             
 if __name__ == "__main__":
-    multiprocessing.set_start_method('spawn')
     #best range is 10000 to 30000
     start=10000
     end=30000
     diff=(end-start)
-    threadcoount=5000
-    offset=round(diff/threadcoount)
-    for x in range(1,threadcoount):
+    #Just make threads a big number, your internet is probably the limiting factor and not the threadcount
+    threadcount=5000
+    offset=round(diff/threadcount)
+    for x in range(1,threadcount):
         t = Process(target=scrape, args=(start+(x*offset),start+((x+1)*offset)))
         t.start()
 
